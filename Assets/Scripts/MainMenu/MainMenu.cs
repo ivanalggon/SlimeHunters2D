0a006473d2ff14776d9d3dd70446206ae3de788f
Iván Sebastián Alguacil González <ivanalguacil11@gmail.com>
2025-03-28 02:08:51 +0100

    update

--- Assets/Scripts/MainMenu/MainMenu.cs
+++ Assets/Scripts/MainMenu/MainMenu.cs
@@ -5,22 +5,18 @@ public class MainMenu : MonoBehaviour
 {
     public void StartPlaying()
     {
-        // Load the game scene
         SceneManager.LoadScene("Game");
     }
     public void ShowCredits()
     {
-        // Load the credits scene
         SceneManager.LoadScene("Credits");
     }
     public void ReturnMainMenu()
     {
-        // Load the main menu scene
         SceneManager.LoadScene("PrincipalMenu");
     }
     public void ExitGame()
     {
-        // Exit the game
         Application.Quit();
     }
 }
